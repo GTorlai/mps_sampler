73621ed5bb57b1d744a6d9b4a47a368fdf166b76
gtorlai <gtorlai@uwaterloo.ca>
2019-02-14 10:53:46 -0800

    Added dmrg module

--- dmrg.h
+++ dmrg.h
@@ -1,5 +1,5 @@
-#ifndef __DMRG_H_
-#define __DMRG_H_
+#ifndef _DMRG_H_
+#define _DMRG_H_
 
 #include <string>
 #include <iostream>
@@ -8,60 +8,141 @@ using namespace itensor;
 
 class DMRG {
     
-    int N_;
-    double h_;
-    MPO H_;
-    SiteSet sites_;
-    std::mt19937 rgen_;
-    
+  int nsites_;
+  MPO Hamiltonian_;
+  SiteSet sites_;
+  std::mt19937 rgen_;
+  MPS psi_;
+  
 public:
     
-    MPS psi_;
-    double gs_energy_;
+  DMRG(int nsites):nsites_(nsites){} 
+  
+  inline MPS GetPsi() {
+      return psi_;
+  }
+  
+  //******** HAMILTONIANS *********//
+  void TransverseFieldIsing(double h,std::string bc){
+    sites_ = SpinHalf(nsites_);
+    auto ampo = AutoMPO(sites_);
+    for(int j = 1; j < nsites_; ++j) {
+      ampo += -4.0,"Sz",j,"Sz",j+1;
+      ampo += -2.0*h,"Sx",j;
+    }
+    ampo += -2.0*h,"Sx",nsites_;
+    if (bc=="pbc"){
+      ampo += -4.0,"Sz",nsites_,"Sz",0;
+    }
+    Hamiltonian_ = MPO(ampo);
+  }
 
-    DMRG(int N,double h):N_(N),h_(h) {} 
-    
-    inline MPS GetPsi() {
-        return psi_;
+  void Heisenberg(std::string bc){
+    sites_ = SpinHalf(nsites_);
+    auto ampo = AutoMPO(sites_);
+    for(int j = 1; j < nsites_; ++j) {
+      ampo += 0.5,"S+",j,"S-",j+1;
+      ampo += 0.5,"S-",j,"S+",j+1;
+      ampo += 1.0,"Sz",j,"Sz",j+1;
     }
-    
-    void BuildHamiltonian(){
-        sites_ = SpinHalf(N_);
-        auto ampo = AutoMPO(sites_);
-        for(int j = 1; j < N_; ++j) {
-            ampo += -4.0,"Sz",j,"Sz",j+1;
-            ampo += -2.0*h_,"Sx",j;
-        }
-        ampo += -2.0*h_,"Sx",N_;
-        H_ = MPO(ampo);
+    if (bc=="pbc"){
+      ampo += 0.5,"S+",nsites_,"S-",1;
+      ampo += 0.5,"S-",nsites_,"S+",1;
+      ampo += 1.0,"Sz",nsites_,"Sz",1;
     }
+    Hamiltonian_ = MPO(ampo);
+  }
 
-    void InitializeMPS(){
-        auto state = InitState(sites_);
-        std::uniform_int_distribution<int> distribution(0,1);
-        for(int i = 1; i <= N_; ++i) {
-            auto ran  = distribution(rgen_);
-            if (ran == 1)
-                state.set(i,"Up");
-            else
-                state.set(i,"Dn");
-        }
-        psi_ = MPS(state);
+  void InitializeRandom(){
+    auto state = InitState(sites_);
+    std::uniform_int_distribution<int> distribution(0,1);
+    for(int i = 1; i <= nsites_; ++i) {
+      auto ran  = distribution(rgen_);
+      if (ran == 1)
+        state.set(i,"Up");
+      else
+        state.set(i,"Dn");
     }
+    psi_ = MPS(state);
+  }
 
-    void run_dmrg(){
-        auto sweeps = Sweeps(5);
-        sweeps.maxm() = 10,20,100,100,200;
-        sweeps.cutoff() = 1E-10;
-        sweeps.niter() = 2;
-        sweeps.noise() = 1E-7,1E-8,0.0;
-        //println(sweeps);
-    
-        // Begin the DMRG calculation
-        auto energy = dmrg(psi_,H_,sweeps,"Quiet");
-        gs_energy_ = energy; 
-        printfln("\nGround State Energy = %.10f",energy/float(N_));
+  void InitializeNeel(){
+    auto state = InitState(sites_);
+    std::uniform_int_distribution<int> distribution(0,1);
+    for(int i = 1; i <= nsites_; ++i) {
+      if(i%2 == 1)
+        state.set(i,"Up");
+      else
+        state.set(i,"Dn");
+      }
+    psi_ = MPS(state);
+  }
+
+
+  void Run(){
+      auto sweeps = Sweeps(10);
+      sweeps.maxm() = 10,20,100,100,200;
+      sweeps.cutoff() = 1E-10;
+      sweeps.niter() = 2;
+      sweeps.noise() = 1E-7,1E-8,0.0;
+  
+      // Begin the DMRG calculation
+      auto energy = dmrg(psi_,Hamiltonian_,sweeps,"Quiet");
+      //gs_energy_ = energy; 
+      printfln("\nGround State Energy = %.10f",energy/float(nsites_));
+  }
+   
+  ITensor BasisState(int & s_value, const ITensor & A){
+    auto index = findtype(A,Site);
+    ITensor s(index);
+    if (s_value == 0){
+      s.set(index(1),1.0);
+      s.set(index(2),0.0);
     }
+    else {
+      s.set(index(1),0.0);
+      s.set(index(2),1.0);
+    }
+    return s;
+  }
+
+  double CollapseWavefunction(std::vector<int> & sigma){
+    ITensor psi_sigma;
+    ITensor s;
+    s = BasisState(sigma[0],psi_.A(1));
+    psi_sigma = psi_.A(1);
+    psi_sigma *= s;
+
+    for (int j=2;j<=nsites_;j++){
+      s = BasisState(sigma[j-1],psi_.A(j));
+      psi_sigma *= psi_.A(j);
+      psi_sigma *= s;
+    }
+    return psi_sigma.real();
+  }
+
+  void PrintFullWavefunction(){
+    if(nsites_>10){
+      std::cout<<"System size too large!"<<std::endl;
+      exit(0);
+    }
+    std::bitset<10> bit;
+    std::vector<int> sigma;
+    for(int i=0; i<1<<nsites_;i++){
+      bit = i;
+      sigma.clear();
+      for(int j=0;j<nsites_;j++){
+        sigma.push_back(bit[nsites_-1-j]);
+      }
+      auto psi_sigma = CollapseWavefunction(sigma); 
+      std::cout<<psi_sigma<<std::endl;
+      //std::cout<<psi_sigma.cplx().real()<<"  "<<psi_sigma.cplx().imag()<<std::endl;
+    }
+    std::cout<<std::endl<<std::endl;
+  }
+
+
+
 };
 
 #endif 
--- main.cc
+++ main.cc
@@ -4,36 +4,49 @@
 #include <random>
 #include <string>
 #include <iomanip>
-#include "sampler.h"
+//#include "sampler.h"
 #include "dmrg.h"
-#include "parameters.h"
+//#include "parameters.h"
 
 using namespace itensor;
     
 
 int main(int argc, char* argv[]) {
 
-    Parameters par;
-    par.ReadParameters(argc,argv);
-    par.PrintParameters(); 
-    int N = par.N_;
-    double h = par.h_;
+  int nsites   = 4;
+  int nsamples = 10000;
+  std::string bc = "obc";
 
-    DMRG solver(N,h); 
-    solver.BuildHamiltonian();
-    solver.InitializeMPS();
-    solver.run_dmrg();
-    MPS psi = solver.GetPsi();
-    printfln("\nGround State Energy = %.10f",solver.gs_energy_/float(N));
+  DMRG dmrg(nsites);
+  dmrg.TransverseFieldIsing(1.0,bc);
+  dmrg.InitializeRandom();
+  dmrg.Run();
+  dmrg.PrintFullWavefunction();
+  
 
-    Sampler sampler(N,h,psi);
-    
-    
-    sampler.get_partial_tensors();
-    for(int k=0; k<ns_; k++){
-        std::cout<< k << std::endl;
-        sampler.sample();
-    }
+
+
+    //Parameters par;
+    //par.ReadParameters(argc,argv);
+    //par.PrintParameters(); 
+    //int N = par.N_;
+    //double h = par.h_;
+
+    //DMRG solver(N,h); 
+    //solver.BuildHamiltonian();
+    //solver.InitializeMPS();
+    //solver.run_dmrg();
+    //MPS psi = solver.GetPsi();
+    //printfln("\nGround State Energy = %.10f",solver.gs_energy_/float(N));
+
+    //Sampler sampler(N,h,psi);
+    //
+    //
+    //sampler.get_partial_tensors();
+    //for(int k=0; k<par.ns_; k++){
+    //    std::cout<< k << std::endl;
+    //    sampler.sample();
+    //}
     
     return 0;
 } 
